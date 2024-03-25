425f42834e09e0278c9cd5b7420c35a3bdae3c4f
ismylsmylv <ismayilovsmyl2020@gmail.com>
2024-03-25 11:17:52 +0400

    sign up flow completed

--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -9,9 +9,11 @@ import {
 } from 'react-native';
 import React, {useState} from 'react';
 import CheckBox from '@react-native-community/checkbox';
+import {useNavigation} from '@react-navigation/native';
 export default function Signup() {
   const [step, setstep] = useState(0);
   const [agreed, setagreed] = useState(false);
+  const navigation = useNavigation();
   return (
     <View style={styles.main}>
       <View style={styles.banner}>
@@ -23,48 +25,155 @@ export default function Signup() {
             : step == 2
             ? 'Set up password'
             : step == 3
-            ? 'Set up password'
-            : step == 4
             ? 'Tell us about yourself'
             : "You're all done, ismylsmylv"}
         </Text>
       </View>
-      <View style={styles.form}>
-        <TextInput
-          style={styles.formInput}
-          placeholder="email"
-          placeholderTextColor={'#6e6d6d'}
-        />
-        <View
-          style={{
-            width: '100%',
-            display: 'flex',
-            alignItems: 'center',
-            gap: 20,
-            marginTop: '40%',
-          }}>
-          <View style={styles.agreement}>
-            <CheckBox
-              tintColor="#e8e8e8"
-              onFillColor="#ff0000"
-              hideBox={false}
-              disabled={false}
-              value={agreed}
-              onValueChange={() => setagreed(!agreed)}
-            />
-            <Text style={{color: 'black'}}>
-              I accept Terms and conditions and Privacy Policy
-            </Text>
+      {step == 0 ? (
+        <View style={styles.form}>
+          <TextInput
+            style={styles.formInput}
+            placeholder="email"
+            placeholderTextColor={'#6e6d6d'}
+          />
+          <View
+            style={{
+              width: '100%',
+              display: 'flex',
+              alignItems: 'center',
+              gap: 20,
+              marginTop: '40%',
+            }}>
+            <View style={styles.agreement}>
+              <CheckBox
+                tintColor="#e8e8e8"
+                onFillColor="#ff0000"
+                hideBox={false}
+                disabled={false}
+                value={agreed}
+                onValueChange={() => setagreed(!agreed)}
+              />
+              <Text style={{color: 'black'}}>
+                I accept Terms and conditions and Privacy Policy
+              </Text>
+            </View>
+            <TouchableOpacity
+              style={[styles.formBtn, {backgroundColor: '#ffffff'}]}
+              onPress={() => {
+                setstep(1);
+                // navigation.navigate('Home');
+              }}>
+              <Text style={{fontSize: 15, color: '#8d39ed'}}>Continue</Text>
+            </TouchableOpacity>
           </View>
+        </View>
+      ) : step == 1 ? (
+        <View style={styles.form}>
+          <TextInput
+            style={styles.formInput}
+            placeholder="username"
+            placeholderTextColor={'#6e6d6d'}
+          />
+          <View
+            style={{
+              width: '100%',
+              display: 'flex',
+              alignItems: 'center',
+              gap: 20,
+              marginTop: '40%',
+            }}>
+            <TouchableOpacity
+              style={[styles.formBtn, {backgroundColor: '#ffffff'}]}
+              onPress={() => {
+                setstep(2);
+                // navigation.navigate('Home');
+              }}>
+              <Text style={{fontSize: 15, color: '#8d39ed'}}>Continue</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      ) : step == 2 ? (
+        <View style={styles.form}>
+          <TextInput
+            style={styles.formInput}
+            placeholder="enter password"
+            placeholderTextColor={'#6e6d6d'}
+          />
+          <TextInput
+            style={styles.formInput}
+            placeholder="re-enter password"
+            placeholderTextColor={'#6e6d6d'}
+          />
+          <View
+            style={{
+              width: '100%',
+              display: 'flex',
+              alignItems: 'center',
+              gap: 20,
+              marginTop: '40%',
+            }}>
+            <TouchableOpacity
+              style={[styles.formBtn, {backgroundColor: '#ffffff'}]}
+              onPress={() => {
+                setstep(3);
+                // navigation.navigate('Home');
+              }}>
+              <Text style={{fontSize: 15, color: '#8d39ed'}}>Continue</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      ) : step == 3 ? (
+        <View style={styles.form}>
+          <TextInput
+            style={styles.formInput}
+            placeholder="first name"
+            placeholderTextColor={'#6e6d6d'}
+          />
+          <TextInput
+            style={styles.formInput}
+            placeholder="last name"
+            placeholderTextColor={'#6e6d6d'}
+          />
+          <View style={styles.formInputDates}>
+            <TextInput placeholder="day" style={styles.formInputDate} />
+            <View style={styles.shortLine} />
+            <TextInput placeholder="month" style={styles.formInputDate} />
+            <View style={styles.shortLine} />
+            <TextInput placeholder="year" style={styles.formInputDate} />
+          </View>
+          <View
+            style={{
+              width: '100%',
+              display: 'flex',
+              alignItems: 'center',
+              gap: 20,
+              marginTop: '40%',
+            }}>
+            <TouchableOpacity
+              style={[styles.formBtn, {backgroundColor: '#ffffff'}]}
+              onPress={() => {
+                setstep(4);
+                // navigation.navigate('Home');
+              }}>
+              <Text style={{fontSize: 15, color: '#8d39ed'}}>Continue</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      ) : (
+        <View style={[styles.form, {justifyContent: 'space-between'}]}>
+          <Text style={styles.completeText}>
+            Your account has been crated succesfully
+          </Text>
           <TouchableOpacity
             style={[styles.formBtn, {backgroundColor: '#ffffff'}]}
             onPress={() => {
-              // navigation.navigate('Home');
+              // setstep(4);
+              navigation.navigate('Welcome');
             }}>
-            <Text style={{fontSize: 15, color: '#8d39ed'}}>Continue</Text>
+            <Text style={{fontSize: 15, color: '#8d39ed'}}>Back to login</Text>
           </TouchableOpacity>
         </View>
-      </View>
+      )}
     </View>
   );
 }
@@ -119,5 +228,30 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     flexDirection: 'row',
+  },
+  formInputDates: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 10,
+    marginTop: 20,
+  },
+  formInputDate: {
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
+    borderColor: '#8d39ed',
+  },
+  shortLine: {
+    width: 20,
+    height: 1,
+    backgroundColor: '#8d39ed',
+  },
+  completeText: {
+    fontSize: 18,
+    color: '#000000',
+    textAlign: 'center',
+    width: '60%',
   },
 });
